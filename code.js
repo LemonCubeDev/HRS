280524b21f18e3143dd9124ddb83a9583b2966cd
LemonCube <110491023+LemonCubeDev@users.noreply.github.com>
2025-07-18 09:30:06 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,60 @@
+onEvent("Welcome", "click", function( ) {
+	playSound("assets/Welcome.mp3", false);
+});
+onEvent("Doors", "click", function( ) {
+	playSound("assets/Doors.mp3", false);
+});
+onEvent("NextObservation", "click", function( ) {
+	playSound("assets/NextObservation.mp3", false);
+});
+onEvent("NextCamping", "click", function( ) {
+	playSound("assets/NextCamping.mp3", false);
+});
+onEvent("NextParking", "click", function( ) {
+	playSound("assets/NextParking.mp3", false);
+});
+onEvent("Music", "click", function( ) {
+	setScreen("BackgroundMusic");
+});
+onEvent("ArrivingObservation", "click", function( ) {
+	playSound("assets/ArrivingObservation.mp3", false);
+});
+onEvent("ArrivingCamping", "click", function( ) {
+	playSound("assets/ArrivingCamping.mp3", false);
+});
+onEvent("ArrivingParking", "click", function( ) {
+	playSound("assets/ArrivingParking.mp3", false);
+});
+onEvent("Thank", "click", function( ) {
+	playSound("assets/Thank.mp3", false);
+});
+onEvent("Footer.Home", "click", function( ) {
+	open("https://github.lemoncube.dev/");
+});
+onEvent("Mingle", "click", function( ) {
+	playSound("assets/Mingle.mp3", false);
+});
+onEvent("jet2", "click", function( ) {
+	playSound("assets/jet2Holidays.mp3", false);
+});
+onEvent("Fart", "click", function( ) {
+	playSound("assets/dry-fart.mp3", false);
+});
+onEvent("21century", "click", function( ) {
+	playSound("assets/21centuryfunk.mp3", false);
+});
+onEvent("Flashbang", "click", function( ) {
+	playSound("assets/flashbanggg.mp3", false);
+});
+onEvent("Eagle", "click", function( ) {
+	playSound("assets/eagle-screech.mp3", false);
+});
+onEvent("Active", "click", function( ) {
+	playSound("assets/Active.mp3", true);
+});
+onEvent("Return", "click", function( ) {
+	setScreen("HighRockSightseeing");
+});
+onEvent("Footer.Music", "click", function( ) {
+	open("https://github.lemoncube.dev/");
+});
